605319b140a368b14a360067a919d02280eba55e
Mark Tuggle <masterofpi11436@gmail.com>
2024-10-17 09:35:37 -0400

    Removed unnecessary PhoneDirectoryController logic

--- app/Http/Controllers/Directory/PhoneDirectoryController.php
+++ app/Http/Controllers/Directory/PhoneDirectoryController.php
@@ -12,70 +12,38 @@ use App\Models\Directory\PhoneDirectory;
 
 class PhoneDirectoryController extends Controller
 {
-
-    // Unprotected route for everyone to view directory and search entries
-    public function indexAll(Request $request)
+    // Unprotected route for everyone to view the directory
+    public function indexAll()
     {
-        $extensions = $this->searchDirectory($request);
-
-        return view('Directory.PhoneDirectory.indexAll', [
-            'extensions' => $extensions,
-            'search' => $request->input('search'),
-        ]);
+        return view('Directory.PhoneDirectory.indexAll');
     }
 
-    // Dashbaord for the Phone Directory manager
-    public function dashboard(Request $request)
+    // Dashboard for the Phone Directory manager
+    public function dashboard()
     {
-        $extensions = $this->searchDirectory($request);
-
-        return view('Directory.PhoneDirectory.dashboard', [
-            'extensions' => $extensions,
-            'search' => $request->input('search'),
-        ]);
+        return view('Directory.PhoneDirectory.dashboard');
     }
 
-    // Perform search by name, title, section, or extension.
-    private function searchDirectory (Request $request)
-    {
-        $search = $request->input('search');
-
-        if ($search) {
-            $extensions = PhoneDirectory::where('name', 'like', '%' . $search . '%')
-                                        ->orWhere('title', 'like', '%' . $search . '%')
-                                        ->orWhere('section', 'like', '%' . $search . '%')
-                                        ->orWhere('extension', 'like', '%' . $search . '%')
-                                        ->orderBy('section')
-                                        ->get();
-        }
-
-        return PhoneDirectory::orderBy('section')->get();
-    }
-
+    // Create new entry
     public function create()
     {
         return view('Directory.PhoneDirectory.create');
     }
 
+    // Edit existing entry
     public function edit($id)
     {
-        // Retrieve the record by its ID
         $phoneDirectory = PhoneDirectory::findOrFail($id);
-
-        // Pass the retrieved record to the Blade view
         return view('Directory.PhoneDirectory.edit', ['phoneDirectory' => $phoneDirectory]);
     }
 
+    // Delete entry
     public function destroy($id)
     {
-        // Find the record and delete it
         $phoneDirectory = PhoneDirectory::findOrFail($id);
         $phoneDirectory->delete();
 
-        // Flash message for successful deletion
         session()->flash('create-edit-delete-message', 'Record deleted successfully!');
-
-        // Redirect to the same page or wherever needed
         return redirect()->back();
     }
 }
