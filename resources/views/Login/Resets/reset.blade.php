c7ae955fa6aee0d1b23ace0f5cc40f85533a9777
Mark <masterofpi11436@gmail.com>
2024-11-09 16:28:05 -0500

    Added CSS for the reset pages

--- resources/views/Login/Resets/reset.blade.php
+++ resources/views/Login/Resets/reset.blade.php
@@ -1,29 +1,41 @@
-<h2>Reset Password</h2>
+@extends('layouts.reset')
 
-<form action="{{ route('login.update') }}" method="POST">
-    @csrf
+@section('title', 'Reset Your Password')
 
-    <input type="hidden" name="token" value="{{ $token }}">
-    <label for="email">Email Address</label>
-    <input type="email" name="email" id="email" required>
+@section('heading', 'Reset Your Password')
 
-    <label for="password">New Password</label>
-    <input type="password" name="password" id="password" required>
+@section('content')
 
-    <label for="password_confirmation">Confirm Password</label>
-    <input type="password" name="password_confirmation" id="password_confirmation" required>
+    <h2>Reset Password</h2>
 
-    <button type="submit">Reset Password</button>
-</form>
+    <form action="{{ route('login.update') }}" method="POST">
+        @csrf
 
-@if (session('status'))
-    <p style="color: green;">{{ session('status') }}</p>
-@endif
+        <input type="hidden" name="token" value="{{ $token }}">
+        <label for="email">Email Address</label>
+        <input type="email" name="email" id="email" required>
 
-@error('email')
-    <p style="color: red;">{{ $message }}</p>
-@enderror
+        <label for="password">New Password</label>
+        <input type="password" name="password" id="password" required>
 
-@error('password')
-    <p style="color: red;">{{ $message }}</p>
-@enderror
+        <label for="password_confirmation">Confirm Password</label>
+        <input type="password" name="password_confirmation" id="password_confirmation" required>
+
+        <button type="submit">Reset Password</button>
+    </form>
+
+    @if (session('status'))
+        <p style="color: green;">{{ session('status') }}</p>
+    @endif
+
+    @error('email')
+        <p style="color: red;">{{ $message }}</p>
+    @enderror
+
+    @error('password')
+        <p style="color: red;">{{ $message }}</p>
+    @enderror
+
+@endsection
+
+
