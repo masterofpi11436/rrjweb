ccfd1373db33fb6fd96c7ece880a4b13ef2b9d99
Mark <masterofpi11436@gmail.com>
2025-01-06 20:11:46 -0500

    Started logins for warehouse application

---
+++ app/Http/Controllers/Login/Custom/WarehouseSupervisorLoginController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Login\Custom;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+
+class WarehouseSupervisorLoginController extends BaseLoginController
+{
+    public function WarehouseSupervisorLoginForm()
+    {
+        return view('Login.Logins.warehouse-supervisor-login');
+    }
+
+    public function login(Request $request)
+    {
+        // Validate login input with ends_with directly in the login method
+        $request->validate([
+            'email' => 'required|email|ends_with:rrjva.org',
+            'password' => 'required',
+        ]);
+
+        // Check if the email exists
+        $user = $this->checkEmailExists($request->email);
+
+        // If user is not found, return error
+        if (!$user) {
+            return redirect()->route('phone.login')->withErrors(['email_not_found' => 'No account found with this email address.']);
+        }
+
+        // Attempt login with phone access or admin access
+        return $this->attemptLogin($request, 'phone.dashboard', function ($user) {
+            return $user->phone == 1 || $user->admin == 1;
+        });
+    }
+
+    public function phoneForgotPasswordForm()
+    {
+        return parent::showForgotPasswordForm('Login.Forgots.phone-forgot-password');
+    }
+
+    public function logout(Request $request, $route = 'phone.login')
+    {
+        // Perform the standard logout
+        Auth::logout();
+
+        // Invalidate the session to delete the cookie
+        $request->session()->invalidate();
+        Cookie::queue(Cookie::forget('XSRF-TOKEN'));
+        Cookie::queue(Cookie::forget('remember_token'));
+
+        // Redirect to the specified application
+        return redirect()->route($route);
+    }
+}
