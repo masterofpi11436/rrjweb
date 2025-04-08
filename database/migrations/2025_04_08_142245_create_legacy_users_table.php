07a33f72ec6fb83c3254b93709f3222d5299b6a3
Mark Tuggle <masterofpi11436@gmail.com>
2025-04-08 16:02:15 -0400

    Added Legacy database structure

---
+++ database/migrations/2025_04_08_142245_create_legacy_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('legacy_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('password');
+            $table->foreignId('role_id')
+                ->nullable()
+                ->constrained('legacy_roles');
+            $table->string('reset_token');
+            $table->dateTime('token_expirey');
+            $table->integer('warehouse_role')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('legacy_users');
+    }
+};
