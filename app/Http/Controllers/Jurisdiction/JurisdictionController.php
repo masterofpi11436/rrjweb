e2f7d605433c8be223e70357c4f96b59ea4dfb00
Mark Tuggle <masterofpi11436@gmail.com>
2025-05-22 14:49:37 -0400

    Started CRUD for Jurisdictions

--- app/Http/Controllers/Jurisdiction/JurisdictionController.php
+++ app/Http/Controllers/Jurisdiction/JurisdictionController.php
@@ -13,4 +13,46 @@ class JurisdictionController extends Controller
     {
         return view('Jurisdiction.jurisdiction.dashboard');
     }
+
+    // CRUD for Jurisdictions
+    public function create()
+    {
+        return view('Jurisdiction.jurisdiction.create');
+    }
+
+    public function edit($id)
+    {
+        $jurisdiction = Jurisdiction::findOrFail($id);
+        return view('Jurisdiction.jurisdiction.edit', ['jurisdiction' => $jurisdiction]);
+    }
+
+    public function destroy($id)
+    {
+        $jurisdiction = Jurisdiction::findOrFail($id);
+        $jurisdiction->delete();
+
+        session()->flash('create-edit-delete-message', 'Record deleted successfully!');
+        return redirect()->back();
+    }
+
+    // CRUD for Jurisdiction Time Logs
+    public function createTimeLog()
+    {
+        return view('Jurisdiction.jurisdiction.create');
+    }
+
+    public function editTimeLog($id)
+    {
+        $jurisdiction = Jurisdiction::findOrFail($id);
+        return view('Jurisdiction.jurisdiction.edit', ['jurisdiction' => $jurisdiction]);
+    }
+
+    public function destroyTimeLog($id)
+    {
+        $jurisdiction = Jurisdiction::findOrFail($id);
+        $jurisdiction->delete();
+
+        session()->flash('create-edit-delete-message', 'Record deleted successfully!');
+        return redirect()->back();
+    }
 }
