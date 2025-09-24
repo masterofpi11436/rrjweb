6e4967bbd8295022cd63cb96e0bd3309f861f138
Mark Tuggle <masterofpi11436@gmail.com>
2025-09-24 15:45:31 -0400

    fixed it

--- app/Http/Controllers/Login/BaseLoginController.php
+++ app/Http/Controllers/Login/BaseLoginController.php
@@ -77,7 +77,7 @@ class BaseLoginController extends Controller
     {
         // Validate the email field
         $request->validate([
-            'email' => 'required|email|ends_with:rrjva.org,RRJVA.ORGicsolutions.com',
+            'email' => 'required|email|ends_with:rrjva.org,RRJVA.ORG,icsolutions.com',
         ]);
 
         // Get the number of failed attempts from the session (default to 0)
