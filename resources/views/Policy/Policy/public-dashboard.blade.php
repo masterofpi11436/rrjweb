d541ecf5dfcd288a43b7da496678dee32871fe47
Mark Tuggle <masterofpi11436@gmail.com>
2026-01-26 15:56:22 -0500

    Removed Edge check

--- resources/views/Policy/Policy/public-dashboard.blade.php
+++ resources/views/Policy/Policy/public-dashboard.blade.php
@@ -12,25 +12,6 @@
     </a>
 </div><br>
 
-@php
-    $ua = request()->header('User-Agent');
-    $isEdge = str_contains($ua, 'Edg') || str_contains($ua, 'Edge');
-    $isIE = str_contains($ua, 'Trident') || str_contains($ua, 'MSIE');
-@endphp
-
-@if ($isEdge || $isIE)
-    <div style="position: fixed; inset: 0; display: flex; align-items: center; justify-content: center; background-color: #ef4444; color: white; text-align: center; padding: 2rem; z-index: 9999;">
-        <div>
-            <p style="font-size: 40px; font-weight: bold;">You have opened this link in Microsoft Edge.</p>
-            <p>This application is not supported in Microsoft Edge.</p>
-            <p style="font-size: 40px;">Please open Google Chrome or Firefox and use this web address:</p>
-            <p style="font-size: 30px;">http://rrjapps/policy-search</p>
-            <p style="margin-top: 1rem;">If you have any problems, please contact Mark in MIU at ext 6035.</p>
-        </div>
-    </div>
-@else
-    @livewire('policy.public-policy-search')
-@endif
-
+@livewire('policy.public-policy-search')
 
 @endsection
