fd6e24a17c6897291cddb4b2e077bc2253e328f4
Mark Tuggle <masterofpi11436@gmail.com>
2026-01-27 08:49:26 -0500

    Model fix 2

--- app/Models/Tablet/Tablet.php
+++ app/Models/Tablet/Tablet.php
@@ -12,7 +12,7 @@ class Tablet extends Model
     protected $table = 'tablet';
 
     protected $fillable = ['inmate_number', 'last_name', 'first_name', 'middle_name', 'date_found',
-                           'is_reported', 'is_filed', 'is_charged', 'is_paid', 'notes'];
+                           'is_reported', 'is_filed', 'is_charged', 'is_paid', 'note'];
 
     protected $casts = ['date_found' => 'date'];
 }
