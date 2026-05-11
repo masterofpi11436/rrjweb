bae5c87ffe1ac7c468dd452d6ae3823e4fa95622
Mark Tuggle <masterofpi11436@gmail.com>
2026-05-11 16:27:05 -0400

    Removed unneeded calls

--- app/Http/Controllers/Policy/BuilderController.php
+++ app/Http/Controllers/Policy/BuilderController.php
@@ -2,14 +2,7 @@
 
 namespace App\Http\Controllers\Policy;
 
-// Base Controller
-use Illuminate\Http\Request;
-
-// Models required
-use Smalot\PdfParser\Parser;
-use App\Models\Policy\Policy;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Storage;
 
 class BuilderController extends Controller
 {
