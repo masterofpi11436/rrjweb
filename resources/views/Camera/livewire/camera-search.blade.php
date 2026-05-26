c8b3df98178659ce2729630cad04947be36dfd4e
Mark Tuggle <masterofpi11436@gmail.com>
2026-05-26 11:52:04 -0400

    Added status color styles

--- resources/views/Camera/livewire/camera-search.blade.php
+++ resources/views/Camera/livewire/camera-search.blade.php
@@ -80,7 +80,24 @@
                     <td>{{ $camera->camera_name }}</td>
                     <td>{{ $camera->camera_type?->label() }}</td>
                     <td>{{ $camera->location }}</td>
-                    <td>{{ $camera->status?->label() }}</td>
+                    <td>
+                        @php
+                            $statusClasses = match($camera->status) {
+                                \App\Enums\CameraStatus::GOOD => 'bg-green-100 text-green-800 border border-green-200',
+                                \App\Enums\CameraStatus::NO_VIDEO => 'bg-red-100 text-red-800 border border-red-200',
+                                \App\Enums\CameraStatus::BLURRY => 'bg-yellow-100 text-yellow-800 border border-yellow-200',
+                                \App\Enums\CameraStatus::IRIS => 'bg-purple-100 text-purple-800 border border-purple-200',
+                                \App\Enums\CameraStatus::ADJUST => 'bg-blue-100 text-blue-800 border border-blue-200',
+                                \App\Enums\CameraStatus::CLEAN => 'bg-cyan-100 text-cyan-800 border border-cyan-200',
+                                \App\Enums\CameraStatus::PENDING_DIGITAL_UPGRADE => 'bg-orange-100 text-orange-800 border border-orange-200',
+                                default => 'bg-gray-100 text-gray-800 border border-gray-200',
+                            };
+                        @endphp
+
+                        <span class="px-2 py-1 text-xs font-semibold rounded-full {{ $statusClasses }}">
+                            {{ $camera->status?->label() }}
+                        </span>
+                    </td>
                     <td>{{ $camera->encoder_switch_location }}</td>
                     <td>{{ $camera->ip_address }}</td>
                     <td><a href="{{route('camera.details', $camera->id)}}">View</a></td>
