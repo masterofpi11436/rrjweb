8f86be6f293b62e484006699160365083a832887
Mark Tuggle <masterofpi11436@gmail.com>
2026-06-02 10:48:44 -0400

    Added pill shaped styling for the In Use columns

--- resources/views/Camera/livewire/camera-future-ip-search.blade.php
+++ resources/views/Camera/livewire/camera-future-ip-search.blade.php
@@ -24,18 +24,38 @@
                         @endif
                     </a>
                 </th>
+                <th>
+                    <a href="#" wire:click.prevent="sortBy('is_used')">
+                        In Use
+                        @if ($sortColumn === 'is_used')
+                            @if ($sortDirection === 'asc') ▲ @else ▼ @endif
+                        @endif
+                    </a>
+                </th>
             </tr>
         </thead>
 
         <tbody>
             @forelse ($suggestions as $camera)
+                @php
+                    $statusClass = match((int) $camera->is_used) {
+                        1 => 'bg-green-100 text-green-800 border border-black rounded-full px-4 py-1 inline-block',
+                        0 => 'bg-red-100 text-red-800 border border-black rounded-full px-4 py-1 inline-block'
+                    };
+                @endphp
+
                 <tr>
                     <td>{{ $camera->camera_number }}</td>
                     <td>{{ $camera->future_ip_address }}</td>
+                    <td>
+                        <span class="{{ $statusClass }}">
+                            {{ $camera->is_used ? 'Yes' : 'No' }}
+                        </span>
+                    </td>
                 </tr>
             @empty
                 <tr>
-                    <td colspan="7">No Camera Record Found.</td>
+                    <td colspan="3">No Camera Record Found.</td>
                 </tr>
             @endforelse
         </tbody>
