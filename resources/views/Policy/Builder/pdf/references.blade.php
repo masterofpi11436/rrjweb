2037984402b8e95d563c3c9c3b4e0cfce1a8423d
Mark Tuggle <masterofpi11436@gmail.com>
2026-08-21 10:41:18 -0400

    Trying to fix styling

--- resources/views/Policy/Builder/pdf/references.blade.php
+++ resources/views/Policy/Builder/pdf/references.blade.php
@@ -2,7 +2,7 @@
     .page-title {
         font-size: 18pt;
         font-weight: bold;
-        text-align: center;
+        text-align: left;
         margin-bottom: 20px;
     }
 
@@ -13,50 +13,63 @@
 
     .aca-table {
         width: 100%;
+        border-collapse: collapse;
+        margin-bottom: 4px;
+    }
+
+    .aca-first {
+        padding-top: 15px;
     }
 
     .aca-left {
-        width: 28%;
+        width: 18%;
         font-weight: bold;
         vertical-align: top;
-        padding-right: 8px;
-        line-height: 1.1;
+        padding-right: 12px;
+        line-height: 1.2;
     }
 
     .aca-right {
-        width: 72%;
+        width: 82%;
         vertical-align: top;
-        text-align: justify;
-        line-height: 1.1;
+        text-align: left;
+        line-height: 1.2;
     }
 
     .paragraph {
         text-align: justify;
-        line-height: 1.1;
+        line-height: 1.2;
+        margin-bottom: 6px;
     }
 
     .bullet {
         margin-left: 20px;
+        line-height: 1.2;
+    }
+
+    .reference-spacing {
+        height: 8px;
+    }
+
+    .first {
+        margin-top: 10005px;
     }
 </style>
 
 <div class="page-title">REFERENCES</div>
-
 @foreach ($policy->references as $reference)
     <div class="reference-title">{{ $reference->reference_title }}</div>
-
     @foreach ($reference->paragraphs as $paragraph)
         @if ($paragraph->aca_reference)
-            <div class="aca-reference"><strong>{{ $paragraph->aca_reference }}</strong></div>
-        @endif
-
-        @if ($paragraph->paragraph)
-            <div class="paragraph">{{ $paragraph->paragraph }}</div>
+            <table class="aca-table"><tr><td class="aca-left">{{ $paragraph->aca_reference }}</td><td class="aca-right">{{ $paragraph->paragraph }}</td></tr></table>
+        @elseif ($paragraph->paragraph)<div class="paragraph">{{ $paragraph->paragraph }}</div>
         @endif
 
         @foreach ($paragraph->bullets as $bullet)
-            <div class="bullet">• {{ $bullet->list['text'] ?? '' }}</div>
+            <div class="bullet">
+                • {{ $bullet->list['text'] ?? '' }}
+            </div>
         @endforeach
+        <div class="reference-spacing"></div>
     @endforeach
-    <div></div>
 @endforeach
